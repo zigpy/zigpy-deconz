7aa18fd1e74dd60ab7e6d24b06aca948d5231c44
damarco <schmidt.d@aon.at>
2019-03-29 20:38:14 +0100

    0.1.2 -> 0.1.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="zigpy-deconz",
-    version="0.1.2",
+    version="0.1.3",
     description="A library which communicates with Deconz radios for zigpy",
     url="http://github.com/zigpy/zigpy-deconz",
     author="Daniel Schmidt",
