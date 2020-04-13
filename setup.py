2e81f54f6f6ea8e0aafd026bde43963f22ec7e66
Alexei Chetroi <lexoid@gmail.com>
2020-04-13 13:51:12 -0400

    Sync-up with zigpy new initialization (#105)
    * Update zigpy dependency.
    * Add barebone config schema.
    Make Watchdog TTL configurable via schema.
    
    * Use device config for UART connection.
    * Implment new zigpy initalization.
    * Update api tests.
    * Update application tests.
    * Update uart tests.
    * Fix logging formatting.
    * Update watchdog schema.

--- setup.py
+++ setup.py
@@ -21,6 +21,6 @@ setup(
     author_email="schmidt.d@aon.at",
     license="GPL-3.0",
     packages=find_packages(exclude=["*.tests"]),
-    install_requires=["pyserial-asyncio", "zigpy-homeassistant>=0.17.0"],
+    install_requires=["pyserial-asyncio", "zigpy>=0.20.a1"],
     tests_require=["pytest", "pytest-asyncio", "asynctest"],
 )
---
+++ zigpy_deconz/config.py
@@ -0,0 +1,17 @@
+import voluptuous as vol
+from zigpy.config import (  # noqa: F401 pylint: disable=unused-import
+    CONF_DEVICE,
+    CONF_DEVICE_PATH,
+    CONFIG_SCHEMA,
+)
+
+CONF_WATCHDOG_TTL = "watchdog_ttl"
+CONF_WATCHDOG_TTL_DEFAULT = 600
+
+CONFIG_SCHEMA = CONFIG_SCHEMA.extend(
+    {
+        vol.Optional(CONF_WATCHDOG_TTL, default=CONF_WATCHDOG_TTL_DEFAULT): vol.All(
+            int, vol.Range(min=180)
+        )
+    }
+)
