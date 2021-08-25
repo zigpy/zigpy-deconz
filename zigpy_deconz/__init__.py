069cd6e2209b9a5eabe016aff1d17a152e153ed3
Alexei Chetroi <Alexei.Chetroi@outlook.com>
2021-08-24 22:51:13 -0400

    0.14.0.dev0 Version bump

--- zigpy_deconz/__init__.py
+++ zigpy_deconz/__init__.py
@@ -2,7 +2,7 @@
 
 # coding: utf-8
 MAJOR_VERSION = 0
-MINOR_VERSION = 13
+MINOR_VERSION = 14
 PATCH_VERSION = "0.dev0"
 __short_version__ = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__ = f"{__short_version__}.{PATCH_VERSION}"
