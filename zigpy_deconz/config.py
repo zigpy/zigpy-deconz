eb37a24f89bb0b4e48e7af65ec5236a15f80e791
Zigbee for Domoticz, a plugin to connect your Zigbee devices to Domoticz <96482536+zigbeefordomoticz@users.noreply.github.com>
2022-03-11 17:56:04 +0100

    Align config with other Zigpy radio libs

--- zigpy_deconz/config.py
+++ zigpy_deconz/config.py
@@ -2,10 +2,20 @@
 
 import voluptuous as vol
 from zigpy.config import (  # noqa: F401 pylint: disable=unused-import
+    CONF_NWK,
     CONF_DEVICE,
-    CONF_DEVICE_PATH,
+    CONF_NWK_KEY,
     CONFIG_SCHEMA,
     SCHEMA_DEVICE,
+    CONF_NWK_PAN_ID,
+    CONF_DEVICE_PATH,
+    CONF_NWK_CHANNEL,
+    CONF_NWK_CHANNELS,
+    CONF_NWK_UPDATE_ID,
+    CONF_NWK_TC_ADDRESS,
+    CONF_NWK_TC_LINK_KEY,
+    CONF_NWK_EXTENDED_PAN_ID,
+    cv_boolean,
 )
 
 CONF_WATCHDOG_TTL = "watchdog_ttl"
