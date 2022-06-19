f9ac47df7d96ebd990a94a5907f879376ca847fa
puddly <32534428+puddly@users.noreply.github.com>
2022-06-19 17:39:43 -0400

    0.18.0.dev0 version bump

--- zigpy_deconz/__init__.py
+++ zigpy_deconz/__init__.py
@@ -2,7 +2,7 @@
 
 # coding: utf-8
 MAJOR_VERSION = 0
-MINOR_VERSION = 17
+MINOR_VERSION = 18
 PATCH_VERSION = "0.dev0"
 __short_version__ = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__ = f"{__short_version__}.{PATCH_VERSION}"
