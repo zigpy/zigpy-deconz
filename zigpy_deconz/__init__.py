321d403abe1d19007d3c1f774d69577cd15fe41c
puddly <32534428+puddly@users.noreply.github.com>
2023-07-03 18:23:02 -0400

    Remove `__version__`

--- zigpy_deconz/__init__.py
+++ zigpy_deconz/__init__.py
@@ -1,8 +1,1 @@
 """Init file for zigpy_deconz."""
-
-# coding: utf-8
-MAJOR_VERSION = 0
-MINOR_VERSION = 21
-PATCH_VERSION = "0"
-__short_version__ = f"{MAJOR_VERSION}.{MINOR_VERSION}"
-__version__ = f"{__short_version__}.{PATCH_VERSION}"
