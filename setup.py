322520e5070fcf02e65ef20f971575d620262970
puddly <32534428+puddly@users.noreply.github.com>
2023-07-03 17:49:24 -0400

    Migrate to `pyproject.toml` and Git versioning (#223)
    * Migrate to pyproject.toml
    
    * Fix pre-commit formatting errors

--- setup.py
+++ setup.py
@@ -1,26 +1,6 @@
-"""Setup module for zigpy-deconz."""
+"""Setup stub for legacy builds."""
 
-import os
+import setuptools
 
-from setuptools import find_packages, setup
-
-import zigpy_deconz
-
-this_directory = os.path.join(os.path.abspath(os.path.dirname(__file__)))
-with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
-    long_description = f.read()
-
-setup(
-    name="zigpy-deconz",
-    version=zigpy_deconz.__version__,
-    description="A library which communicates with Deconz radios for zigpy",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="http://github.com/zigpy/zigpy-deconz",
-    author="Daniel Schmidt",
-    author_email="schmidt.d@aon.at",
-    license="GPL-3.0",
-    packages=find_packages(exclude=["tests"]),
-    install_requires=["zigpy>=0.54.0"],
-    tests_require=["pytest", "asynctest"],
-)
+if __name__ == "__main__":
+    setuptools.setup()
